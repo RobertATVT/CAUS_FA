6c0a1068715c46e121f84300fb39d87b56e603ad
Robert A Vaughan <robertav@vt.edu>
2018-03-13 12:42:14 -0400

    PHP Code Changes
    Trying to fix changes made to  employee_asset_view and filter-content (Done with Matt)

--- wp-content/plugins/CAUS-Fixed-Assets/includes/employee_asset_view.php
+++ wp-content/plugins/CAUS-Fixed-Assets/includes/employee_asset_view.php
@@ -18,8 +18,7 @@ function causfa_load_employee_view() {
     $current_user = wp_get_current_user();
     $result_user = $wpdb->get_row('SELECT * FROM causfa_custodians WHERE Email = "'.$current_user->user_email.'";');
     $output = apply_filters('causfa_employee_info', $result_user);
-    $response = wp_remote_get(plugins_url('assets/html/asset_header_template.html', CAUSFA_PLUGIN_URL));
-    $output = $output.wp_remote_retrieve_body($response);
+    $output = $output.(file_get_contents(plugin_dir_path(CAUSFA_PLUGIN_URL).'/assets/html/asset_header_template.html', true ));
 
     $results_assets = $wpdb->get_results('SELECT * FROM causfa_banner WHERE FZVFORG_CUSTODIAN = "'.$result_user->Name.'";');
     $value_total = 0.00;
@@ -36,8 +35,7 @@ function causfa_load_employee_view() {
     }
     $output = $output.(apply_filters('causfa_employee_asset_total', $value_total, $missing_total));
     //Gets the html for the modals and puts it at the bottom of the page.
-    $response = wp_remote_get(plugins_url('assets/html/modal.html', CAUSFA_PLUGIN_URL));
-    $modals = wp_remote_retrieve_body($response);
+    $modal = (file_get_contents(plugin_dir_path(CAUSFA_PLUGIN_URL).'/assets/html/modal.html', true ));
     $modals = str_replace('[PID]', "'".$result_user->PID."'", $modals);
     $output = $output.$modals;
     return $output;
