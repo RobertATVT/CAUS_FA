eefa5d21f03c509bab0e8392c1ceb323c3b66a38
Matthew Jezierski <mattwj6@vt.edu>
2018-03-15 09:46:12 -0400

    Improved autocomplete function
    Changed autocomplete function sql query to only pull matching PIDs instead of all PIDS and filtering.

--- wp-content/plugins/CAUS-Fixed-Assets/includes/autocomplete.php
+++ wp-content/plugins/CAUS-Fixed-Assets/includes/autocomplete.php
@@ -11,14 +11,14 @@ function causfa_autocomplete_PID() {
 // These values may have been gotten from a database.
 // We'll use a simple array just to show this example.
     global $wpdb;
-    $values = $wpdb->get_col('SELECT PID FROM causfa_custodians', 0);
-    $output = array();
-    if ($query) {
-        foreach($values as $key => $value) {
-            if (stripos($value, $query) !== false) {
-                array_push($output, $values[$key]);
-            }
-        }
-    }
-    wp_send_json($output);
+    $values = $wpdb->get_col('SELECT PID FROM causfa_custodians WHERE PID LIKE "%'.$query.'%"', 0);
+//    $output = array();
+//    if ($query) {
+//        foreach($values as $key => $value) {
+//            if (stripos($value, $query) !== false) {
+//                array_push($output, $values[$key]);
+//            }
+//        }
+//    }
+    wp_send_json($values);
 }
