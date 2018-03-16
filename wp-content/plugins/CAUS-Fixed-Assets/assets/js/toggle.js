afc3ccdc989258297787c8a78e5f0831d7c8e19b
Matthew Jezierski <mattwj6@vt.edu>
2018-03-16 10:01:13 -0400

    HTML, Javascript, and PHP changes
    Created handler for first time page view to get custodian info into causfa_custodian table. Change + to - on toggle click and vice versa. Added pending type to asset status.

--- wp-content/plugins/CAUS-Fixed-Assets/assets/js/toggle.js
+++ wp-content/plugins/CAUS-Fixed-Assets/assets/js/toggle.js
@@ -1,4 +1,10 @@
 function faa_asset_more_info(clicked_id) {
     var id = 'asset-' + clicked_id;
     document.getElementById(id).classList.toggle("faa-asset-show");
+    var icon = document.getElementById(clicked_id).innerText;
+    if (icon == '+') {
+        document.getElementById(clicked_id).innerText = '-';
+    } else {
+        document.getElementById(clicked_id).innerText = '+';
+    }
 }
