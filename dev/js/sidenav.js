49001b8278305d1f144ef17b5d414cce26dd7e75
Robert A Vaughan <robertav@vt.edu>
2018-03-21 11:58:15 -0400

    SideNav test 4
    Changes to get header height for wordpress header and push the sidebar down to that

--- dev/js/sidenav.js
+++ dev/js/sidenav.js
@@ -1,12 +1,14 @@
-    function openNav() {
-        document.getElementById("mySidenav").style.width = "250px";
-        document.getElementById("main").style.marginLeft = "250px";
-        document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
-    }
+function openNav() {
+    var headerHeight = $('header').outerHeight();
+    document.getElementById("mySidenav").style.top = headerHeight;
+    document.getElementById("mySidenav").style.width = "300px";
+    document.getElementById("main").style.marginLeft = "300px";
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
 
     /* Set the width of the side navigation to 0 and the left margin of the page content to 0, and the background color of body to white */
-    function closeNav() {
-        document.getElementById("mySidenav").style.width = "0";
-        document.getElementById("main").style.marginLeft = "0";
-        document.body.style.backgroundColor = "white";
-    }
+function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.body.style.backgroundColor = "white";
+}
