f91a8bd403592f8fde8de3f04ee48775a6f7497d
Matthew Jezierski <mattwj6@vt.edu>
2018-03-22 11:14:58 -0400

    Removed enqueue of bootstrap.css and bootstrap.js

--- wp-content/plugins/CAUS-Fixed-Assets/enqueue/enqueue.php
+++ wp-content/plugins/CAUS-Fixed-Assets/enqueue/enqueue.php
@@ -11,7 +11,7 @@
 function causfa_enqueue() {
 
     //Styles
-    wp_register_style('causfa_bootstrap', plugins_url( 'assets/css/bootstrap.css', CAUSFA_PLUGIN_URL));
+    //wp_register_style('causfa_bootstrap', plugins_url( 'assets/css/bootstrap.css', CAUSFA_PLUGIN_URL));
    // wp_register_style('causfa_bootstrap_style', plugins_url('assets/css/style.css', CAUSFA_PLUGIN_URL));
     wp_register_style('causfa_bootstrap_style4', plugins_url('assets/css/style4.css', CAUSFA_PLUGIN_URL));
    // wp_register_style('causfa_purecss_style', 'https://unpkg.com/purecss@1.0.0/build/pure-min.css');
@@ -19,7 +19,7 @@ function causfa_enqueue() {
     wp_register_style('causfa_materialize_style', plugins_url('assets/css/materialize.css', CAUSFA_PLUGIN_URL));
     wp_register_style('causfa_material_icons', 'https://fonts.googleapis.com/icon?family=Material+Icons');
 
-    wp_enqueue_style('causfa_bootstrap');
+    //wp_enqueue_style('causfa_bootstrap');
    // wp_enqueue_style('causfa_bootstrap_style');
     wp_enqueue_style('causfa_bootstrap_style4');
    // wp_enqueue_style('causfa_purecss_style');
@@ -28,7 +28,7 @@ function causfa_enqueue() {
     wp_enqueue_style('causfa_material_icons');
 
     //Scripts
-    wp_register_script('causfa_bootstrap_script', plugins_url( 'assets/js/bootstrap.js', CAUSFA_PLUGIN_URL), array('jquery'), false, true);
+    //wp_register_script('causfa_bootstrap_script', plugins_url( 'assets/js/bootstrap.js', CAUSFA_PLUGIN_URL), array('jquery'), false, true);
     wp_register_script('causfa_toggle', plugins_url( 'assets/js/toggle.js', CAUSFA_PLUGIN_URL), array('jquery'), false, true);
     wp_register_script('causfa_modal', plugins_url('assets/js/modal.js', CAUSFA_PLUGIN_URL), array('jquery'), false, true);
     wp_register_script('causfa_main', plugins_url('assets/js/main.js', CAUSFA_PLUGIN_URL), array('jquery'), false, true);
@@ -39,7 +39,7 @@ function causfa_enqueue() {
 
     wp_enqueue_script('jquery');
     wp_enqueue_script('causfa_toggle');
-    wp_enqueue_script('causfa_bootstrap_script');
+    //wp_enqueue_script('causfa_bootstrap_script');
     wp_enqueue_script('causfa_modal');
     wp_enqueue_script('causfa_main');
     wp_enqueue_script('causfa_autocomplete');
