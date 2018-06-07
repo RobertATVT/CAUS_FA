a808149c291882f19264a63535811dcac4b1de15
Matthew Jezierski <mattwj6@vt.edu>
2018-06-07 12:45:53 -0400

    Deletions and changed forms php and js
    Deleted old files and changed structure of pdf form creation

---
+++ assets/js/generate-form.js
@@ -0,0 +1,8 @@
+function generateHomeUse(data) {
+    doc = new jsPDF();
+    alert(JSON.stringify(doc.getFontList()));
+}
+function generateOffieUse(data) {
+    doc = new jsPDF();
+    alert(JSON.stringify(doc.getFontList()));
+}
