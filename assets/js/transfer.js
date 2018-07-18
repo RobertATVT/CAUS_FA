4baa53cac686556d3a2ff354c4b1a7a72e7940cc
Matthew Jezierski <mattwj6@vt.edu>
2018-07-18 12:49:52 -0400

    Bulk Modal dropdown functionality

--- assets/js/transfer.js
+++ assets/js/transfer.js
@@ -25,6 +25,7 @@ function transferModalLoad(elementID) {
         var PID_options = data['PIDs'];
         var Name_options = data['Names'];
         var list_fill = new Array();
+        var PIDs = document.getElementById('PIDs');
         for (var i = 0; i < PID_options.length; i++) {
             var list_item = new Array();
             list_item[0] = Name_options[i];
@@ -35,18 +36,57 @@ function transferModalLoad(elementID) {
             option.setAttribute('data_value', PID_options[i]);
             PIDs.appendChild(option);
         }
-        transferModalLoadFill('recipient-name', list_fill);
+        transferModalLoadFill(elementID, list_fill);
         // RN_# For Loop goes here
     });
 }
 function transferModalLoadFill(element,list_fill) {
-    new Awesomplete(document.getElementById(element), {list:list_fill});
+    for (var i = 0; i < element.length; i++) {
+        new Awesomplete(document.getElementById(element[i]), {list:list_fill});
+    }
 }
 
 // End Transfer Modal Functions
 //Bulk Transfer Modal Functions
 function bulkTransferModalRequested() {
-    alert('Bulk Transfer Requested');
+    jQuery('#bulk-transfer-items').html('');
+    var ids = [];
+    var tags = [];
+    var descriptions = [];
+    var selected = jQuery(" input:checkbox:checked");
+    for( var i = 0; i < selected.length; i++) {
+        var id = selected[i].id.split('-')[2];
+        tags.push(jQuery("#asset-tag-" + id).html());
+        descriptions.push(jQuery("#asset-desc-" + id).html());
+    }
+    var assets = '';
+    for (i = 0; i < tags.length; i++) {
+        var asset = '<div class="center cl t20 form-group" style="padding: 0px;">' +
+            '    <div class="cl m3 t20"><span class="asset-tag-[ID]">' + tags[i] + '</span></div>' +
+            '    <div class="cl m7 t20 shorten-text"><span class="asset-description-[ID]">' + descriptions[i] + '</span></div>' +
+            '    <div class="cl m7 t20">' +
+            '        <input type="text" class="awesomplete" id="recipient-name-[ID]" list="PIDs">' +
+            '        <input type="hidden" id="transferIndex" value="" />' +
+            '    </div>' +
+            '    <div class="cl m3 t20">' +
+            '        <input id="same-reciever-[ID]" type="checkbox" value="same-reciever-[ID]" class="filled-in admin-block-line-b">' +
+            '        <label for="same-reciever-[ID]"></label>' +
+            '    </div>' +
+            '</div>';
+        asset = asset.split('[ID]').join(i);
+        assets = assets + asset;
+    }
+    jQuery('#bulk-transfer-items').html(assets);
+    var recipients = jQuery("[id^=recipient-name-]");
+    for (i = 0; i < recipients.length; i++) {
+        var recipient = recipients[i];
+        var id = recipient.id;
+        ids.push(id);
+    }
+    transferModalLoad(ids);
+    jQuery('#bulk-transferModal').modal();
+    jQuery('#bulk-transferModal').modal('open');
+
 }
 //End Bulk Transfer Modal Functions
 
@@ -75,31 +115,7 @@ function transferAsset(PID_dest) {
 }
 
 function bulkTransferAsset(PID_dest) {
-    ptags = jQuery('#transferModal').find('.asset-tag').html().split(', ');
-    var form = {
-        action: 'causfa_transfer_asset',
-        dest: PID_dest,
-        ptag: ptags,
-        type: 0
-    };
-    jQuery.post(causfa_action_obj.ajax_url, form, function(data) {
-        if(data['status'] == 1) {
-            var id = jQuery('#transferModal').find('#transferIndex').val();
-            id = id.split(', ');
-            for (var i = 0; i < id.length; i++) {
-                var status = jQuery(('#status-' + id[i]));
-                status.html('<div class="asset-status asset-pending">Pending Transfer</div>');
-                jQuery(('#transfer-' + id[i])).attr('onclick', 'modalRequestedOnPendingAsset(this.id)');
-                jQuery(('#surplus-' + id[i])).attr('onclick', 'modalRequestedOnPendingAsset(this.id)');
-                jQuery(('#asset-select-' + id[i])).prop('checked', false);
-            }
-            jQuery('#transferModal').modal('close');
-            jQuery('#responseModal').find('#modal-response-title').text('Transfer Request Submitted');
-            jQuery('#responseModal').find('#modal-response-alert').text(data['message']);
-            jQuery('#responseModal').modal();
-            jQuery('#responseModal').modal('open');
-        }
-    });
+
 }
 
 
