8cf66a352c52c4d03c801e07d564b88a455bf143
Matthew Jezierski <mattwj6@vt.edu>
2018-08-23 11:18:44 -0400

    Oracle functionality

--- includes/oracle.php
+++ includes/oracle.php
@@ -11,14 +11,26 @@ function causfa_oracle_full_org() {
     $conn = oci_connect("FIN_FA_CAUS", "FdgfFcDsG_6NsLFpe_2m", $PROD);
     if (!$conn) {
         $m = oci_error();
-        $output = $m['message']."\n";
+        print $m['message']."\n";
         exit;
     }
     else {
-        $output ="Connected to Oracle!";
+        $query = 'select * from BANINST1.FZVFORG';
+        $stid = oci_parse($conn, $query);
+        $r = oci_execute($stid);
+// Fetch each row in an associative array
+        print '<table border="1">';
+        while ($row = oci_fetch_array($stid, OCI_RETURN_NULLS+OCI_ASSOC)) {
+            print '<tr>';
+            foreach ($row as $item) {
+                print '<td>'.($item !== null ? htmlentities($item, ENT_QUOTES) : '&nbsp').'</td>';
+            }
+            print '</tr>';
+        }
+        print '</table>';
+
     }
 
 // Close the Oracle connection
     oci_close($conn);
-    return $output;
 }
