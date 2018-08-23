74790d1e3c7a08d623fadf0359979259447df941
Matthew Jezierski <mattwj6@vt.edu>
2018-08-23 11:04:27 -0400

    Changes to oracle connection

--- includes/oracle.php
+++ includes/oracle.php
@@ -5,20 +5,17 @@
  * Date: 8/21/18
  * Time: 1:46 PM
  */
-function causfa_test_oracle() {
-    $output = "Let's connect to Oracle!";
+function causfa_oracle_full_org() {
     // Create connection to Oracle
     $PROD="(DESCRIPTION=(ADDRESS=(PROTOCOL=tcp)(PORT=1251)(HOST=prod-ha.db.vt.edu))(CONNECT_DATA=(UR=A)(SERVICE_NAME=PROD)))";
-    $output = $output."  DB String is set yo!";
     $conn = oci_connect("FIN_FA_CAUS", "FdgfFcDsG_6NsLFpe_2m", $PROD);
-    $output = $output."   DB Connection is Not made";
     if (!$conn) {
         $m = oci_error();
-        $output = $output.$m['message']."\n";
+        $output = $m['message']."\n";
         exit;
     }
     else {
-        $output = $output."Connected to Oracle!";
+        $output ="Connected to Oracle!";
     }
 
 // Close the Oracle connection
--- index.php
+++ index.php
@@ -80,5 +80,5 @@ add_action( 'wp_ajax_causfa_add_note', 'causfa_add_note');
 
 //Shortcode
 add_shortcode( 'causfa', 'causfa_load_employee_view'); //shortcode for the main page of the app
-add_shortcode( 'causfa_test', 'causfa_test_oracle'); //shortcode for a dev space to test functionality
+add_shortcode( 'causfa_test', 'causfa_oracle_full_org'); //shortcode for a dev space to test functionality
 add_shortcode( 'causfa_admin', 'causfa_load_admin_view'); // shortcode for the admin page of the app
