bec4919f23934b42ca2617a2ba7f8c4ae41be625
Matthew Jezierski <mattwj6@vt.edu>
2018-08-23 12:14:58 -0400

    Testing ajax call setup for oracle updated

--- includes/oracle.php
+++ includes/oracle.php
@@ -7,8 +7,6 @@
  */
 
 function causfa_oracle_full_org() {
-    $output = 'Test';
-    wp_send_json($output);
     // Create connection to Oracle
     $PROD="(DESCRIPTION=(ADDRESS=(PROTOCOL=tcp)(PORT=1251)(HOST=prod-ha.db.vt.edu))(CONNECT_DATA=(UR=A)(SERVICE_NAME=PROD)))";
     $conn = oci_connect("FIN_FA_CAUS", "FdgfFcDsG_6NsLFpe_2m", $PROD);
@@ -23,15 +21,17 @@ function causfa_oracle_full_org() {
         $stid = oci_parse($conn, $query);
         $r = oci_execute($stid);
         $output = causfa_oracle_compare($stid);
+        wp_send_json($output);
     }
 // Close the Oracle connection
     oci_close($conn);
-    wp_send_json($output);
+
 }
 
 function causfa_oracle_compare($stid) {
     global $wpdb;
     $assets = $wpdb->get_results('SELECT * FROM causfa_banner');
+    $output = 'never finding it';
     while ($row = oci_fetch_array($stid, OCI_RETURN_NULLS+OCI_ASSOC)) {
         for($i = 0; $i < count($assets); $i++) {
             if ($assets[$i]['FZVFORG_PTAG'] === $row['FZVFORG_PTAG']) {
