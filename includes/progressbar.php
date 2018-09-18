abddf8af7e14def087859ae9ff875e8124e08284
Matthew Jezierski <mattwj6@vt.edu>
2018-09-18 13:49:22 -0400

    Testing something

--- includes/progressbar.php
+++ includes/progressbar.php
@@ -7,11 +7,12 @@
  */
 function causfa_progressbar() {
 
+    ini_set('zlib.output_compression',0);
+    ini_set('implicit_flush',1);
     header('Content-Type: text/event-stream');
 // recommended to prevent caching of event data.
     header('Cache-Control: no-cache');
-    myEchoFlush_init();
-    //header('X-Accel-Buffering: no');
+    ob_implicit_flush(1);
 //LONG RUNNING TASK
     for($i = 1; $i <= 10; $i++) {
         send_message($i, 'on iteration ' . $i . ' of 10' , $i*10);
@@ -28,16 +29,7 @@ function send_message($id, $message, $progress) {
     echo "id: $id" . PHP_EOL;
     echo "data: " . json_encode($d) . PHP_EOL;
     echo PHP_EOL;
-    ob_flush();
-    flush();
-}
-function myEchoFlush_init() {
-    ini_set('zlib.output_compression', 0);
-    ini_set('output_buffering', 'Off');
-    ini_set('output_handler', '');
-    ini_set('implicit_flush', 1);
-    ob_implicit_flush(1);
-    ob_end_clean();
-    //header('Content-Encoding: none;');
-
+    echo str_repeat(' ',1024*64);
+//    ob_flush();
+//    flush();
 }
