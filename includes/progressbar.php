b6791f4e0030b809077dc8e9e646985ceeb975b3
Matthew Jezierski <mattwj6@vt.edu>
2018-09-18 14:19:20 -0400

    Again

--- includes/progressbar.php
+++ includes/progressbar.php
@@ -6,21 +6,30 @@
  * Time: 3:14 PM
  */
 function causfa_progressbar() {
+
     ini_set('zlib.output_compression',0);
     ini_set('implicit_flush',1);
     header('Content-Type: text/event-stream');
-    // recommended to prevent caching of event data.
+// recommended to prevent caching of event data.
     header('Cache-Control: no-cache');
     ob_implicit_flush(1);
-    //LONG RUNNING TASK
+//LONG RUNNING TASK
     for($i = 1; $i <= 10; $i++) {
-        send_message('on iteration ' . $i . ' of 10' , $i*10);
+        send_message($i, 'on iteration ' . $i . ' of 10' , $i*10);
+
         sleep(1);
     }
-    send_message('Process complete','CLOSE');
+
+    send_message('CLOSE', 'Process complete');
+
 }
-function send_message($message, $progress) {
-    $d = array('message' => $message , 'progress' => $progress);
+function send_message($id, $message, $progress = null) {
+    if ($progress === null) {
+        $d = ARRAY('message' => $message);
+    } else {
+        $d = array('message' => $message , 'progress' => $progress);
+    }
+    echo "id: $id" . PHP_EOL;
     echo "data: " . json_encode($d) . PHP_EOL;
     echo PHP_EOL;
     echo str_repeat(' ',1024*64);
