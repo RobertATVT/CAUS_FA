397d9e7afa55a2ead08e26ccd7bb0b1b7abe3ded
Matthew Jezierski <mattwj6@vt.edu>
2018-09-17 22:01:47 -0400

    Oracle.php change
    Changed row count to be pulled from oci_num_rows

--- includes/oracle.php
+++ includes/oracle.php
@@ -29,33 +29,33 @@ if (!$conn) {
 // Close the Oracle connection
 oci_close($conn);
 
-//$total = count($stid);
-//$count = 0;
-//
-//global $wpdb;
-//$assets = $wpdb->get_results('SELECT * FROM causfa_banner ORDER BY FZVFORG_PTAG');
-//$output = 'never finding it';
-//while ($row = oci_fetch_array($stid, OCI_RETURN_NULLS+OCI_ASSOC)) {
-//    $found = false;
-//    for ($i = 0; $i < count($assets); $i++) {
-//        $found = false;
-//        if ($assets[$i]->FZVFORG_PTAG === $row['FZVFORG_PTAG']) {
-//            $output = causfa_oracle_compare_custodian($row, $assets[$i]);
-//            $output = $output . causfa_oracle_compare_location($row, $assets[$i]);
-//            $output = $output . causfa_oracle_compare_org($row, $assets[$i]);
-//            $output = $output . causfa_oracle_compare_ownership($row, $assets[$i]);
-//            array_splice($assets, $i, 1);
-//            $found = true;
-//        }
-//    }
-//    $count++;
-//    $percent = intval($count/$total * 100)."%";
-//    echo '<script>
-//    parent.document.getElementById("progressbar").innerHTML="<div style=\"width:'.$percent.';background:linear-gradient(to bottom, rgba(125,126,125,1) 0%,rgba(14,14,14,1) 100%); ;height:35px;\">&nbsp;</div>";
-//    parent.document.getElementById("FA_LoadPercent").innerHTML="'.$percent.'";</script>';
-//
-//    ob_flush();
-//    flush();
-//}
+$total = oci_num_rows($stid);
+$count = 0;
+
+global $wpdb;
+$assets = $wpdb->get_results('SELECT * FROM causfa_banner ORDER BY FZVFORG_PTAG');
+$output = 'never finding it';
+while ($row = oci_fetch_array($stid, OCI_RETURN_NULLS+OCI_ASSOC)) {
+    $found = false;
+    for ($i = 0; $i < count($assets); $i++) {
+        $found = false;
+        if ($assets[$i]->FZVFORG_PTAG === $row['FZVFORG_PTAG']) {
+            $output = causfa_oracle_compare_custodian($row, $assets[$i]);
+            $output = $output . causfa_oracle_compare_location($row, $assets[$i]);
+            $output = $output . causfa_oracle_compare_org($row, $assets[$i]);
+            $output = $output . causfa_oracle_compare_ownership($row, $assets[$i]);
+            array_splice($assets, $i, 1);
+            $found = true;
+        }
+    }
+    $count++;
+    $percent = intval($count/$total * 100)."%";
+    echo '<script>
+    parent.document.getElementById("progressbar").innerHTML="<div style=\"width:'.$percent.';background:linear-gradient(to bottom, rgba(125,126,125,1) 0%,rgba(14,14,14,1) 100%); ;height:35px;\">&nbsp;</div>";
+    parent.document.getElementById("FA_LoadPercent").innerHTML="'.$percent.'";</script>';
+
+    ob_flush();
+    flush();
+}
 
 session_destroy();
