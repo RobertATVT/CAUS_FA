e70a87f64e7b4d5ea566d199e25fc2f5793babaf
Matthew Jezierski <mattwj6@vt.edu>
2018-09-18 13:26:31 -0400

    And Again

--- includes/progressbar.php
+++ includes/progressbar.php
@@ -22,13 +22,11 @@ function causfa_progressbar() {
 
 }
 function send_message($id, $message, $progress) {
-    ob_start();
     $d = array('message' => $message , 'progress' => $progress);
 
     echo "id: $id" . PHP_EOL;
     echo "data: " . json_encode($d) . PHP_EOL;
     echo PHP_EOL;
-    ob_end_flush();
-    //ob_flush();
+    ob_flush();
     flush();
 }
