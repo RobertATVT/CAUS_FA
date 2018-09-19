2fa436ac85f4e0448952eb35606fc3b649262cd9
Matthew Jezierski <mattwj6@vt.edu>
2018-09-19 14:36:23 -0400

    change to looping through assets from banner

--- includes/oracle.php
+++ includes/oracle.php
@@ -27,24 +27,35 @@ function casufa_oracle_org_report()
         $query = "select * from BANINST1.FZVFORG order by FZVFORG_PTAG";
         $stid = oci_parse($conn, $query);
         $r = oci_execute($stid);
+        $oracle_assets = oci_fetch_all($stid);
+        oci_close($conn);
+        causfa_oracle_compare($oracle_assets);
     }
-    // Close the Oracle connection
-    $total = oci_num_rows($stid);
-    oci_close($conn);
-
+}
+function send_message($id, $message, $progress) {
+    $d = array('message' => $message , 'progress' => $progress);
+    echo "id: $id" . PHP_EOL;
+    echo "data: " . json_encode($d) . PHP_EOL;
+    echo PHP_EOL;
+    echo str_repeat(' ',1024*64);
+}
+function causfa_oracle_compare($oracle) {
     $count = 0;
+    $total = count($oracle);
     global $wpdb;
     $assets = $wpdb->get_results('SELECT * FROM causfa_banner ORDER BY FZVFORG_PTAG');
-    while ($row = oci_fetch_array($stid, OCI_RETURN_NULLS + OCI_ASSOC)) {
+    for ($i = 0; $i < $total; $i++) {
+        $row = $oracle[$i];
         $found = false;
-        for ($i = 0; $i < count($assets); $i++) {
+        for ($j = 0; $j < count($assets); $j++) {
             $found = false;
             if ($assets[$i]->FZVFORG_PTAG === $row['FZVFORG_PTAG']) {
                 $output = causfa_oracle_compare_custodian($row, $assets[$i]);
                 $output = $output . causfa_oracle_compare_location($row, $assets[$i]);
                 $output = $output . causfa_oracle_compare_org($row, $assets[$i]);
                 $output = $output . causfa_oracle_compare_ownership($row, $assets[$i]);
-                array_splice($assets, $i, 1);
+                array_splice($assets, $j, 1);
+                array_splice($oracle, $i, 1);
                 $found = true;
             }
         }
@@ -53,13 +64,6 @@ function casufa_oracle_org_report()
         send_message($row['FZVFORG_PTAG'], $count. ' of '.$total. " = ".$found, $percent);
     }
     send_message(0,'CLOSE', 'Process complete');
-}
-function send_message($id, $message, $progress) {
-    $d = array('message' => $message , 'progress' => $progress);
-    echo "id: $id" . PHP_EOL;
-    echo "data: " . json_encode($d) . PHP_EOL;
-    echo PHP_EOL;
-    echo str_repeat(' ',1024*64);
 }
 function causfa_oracle_compare_custodian($oracle, $asset) {
 
