d7c8977640eeead55b92851f42be261c6ce66af6
Matthew Jezierski <mattwj6@vt.edu>
2018-09-22 20:05:50 -0400

    Oracle check for custodian

--- includes/oracle.php
+++ includes/oracle.php
@@ -46,23 +46,28 @@ function causfa_oracle_compare($oracle) {
     global $wpdb;
     $assets = $wpdb->get_results('SELECT * FROM causfa_banner ORDER BY FZVFORG_PTAG');
     $total_local = count($assets);
-    $wpdb->insert(
-        'causfa_exception_reports',
-        array(
-            'TOTAL_BANNER' => $total,
-            'TOTAL_LOCAL' => $total_local,
-        )
-    );
-    $id = $wpdb->insert_id;
+    $exceptions_list = array();
     foreach ($oracle as $key => $row) {
         $found = false;
         for ($i = 0; $i < count($assets); $i++) {
             $found = false;
             if ($assets[$i]->FZVFORG_PTAG == $row['FZVFORG_PTAG']) {
-                $output = causfa_oracle_compare_custodian($row, $assets[$i]);
-                $output = $output . causfa_oracle_compare_location($row, $assets[$i]);
-                $output = $output . causfa_oracle_compare_org($row, $assets[$i]);
-                $output = $output . causfa_oracle_compare_ownership($row, $assets[$i]);
+                $result = causfa_oracle_compare_custodian($row, $assets[$i]);
+                if ($result !== null) {
+                    array_push($exceptions_list, $result);
+                }
+                $result = causfa_oracle_compare_location($row, $assets[$i]);
+                if ($result !== null) {
+                    array_push($exceptions_list, $result);
+                }
+                $result = causfa_oracle_compare_org($row, $assets[$i]);
+                if ($result !== null) {
+                    array_push($exceptions_list, $result);
+                }
+                $result = causfa_oracle_compare_ownership($row, $assets[$i]);
+                if ($result !== null) {
+                    array_push($exceptions_list, $result);
+                }
                 array_splice($assets, $i, 1);
                 array_splice($oracle, $key, 1);
                 $found = true;
@@ -70,22 +75,7 @@ function causfa_oracle_compare($oracle) {
             }
         }
         if (!$found) {
-            $result = $wpdb->get_row("SELECT * FROM causfa_exception_reports WHERE ID = ".$id.";");
-            $exceptions_list = maybe_unserialize($result->EXCEPTIONS);
-            if ($exceptions_list === null) {
-                $exceptions_list = array(
-                    $row['FZVFORG_PTAG']." was not found in the local database"
-                );
-            } else {
-                array_push($exceptions_list, ($row['FZVFORG_PTAG'].' was not found in the local databse'));
-            }
-            $wpdb->update(
-                'causfa_exception_reports',
-                array(
-                    'EXCEPTIONS' => maybe_serialize($exceptions_list)
-                ),
-                array('ID' => $id)
-            );
+            array_push($exceptions_list, ($row['FZVFORG_PTAG'].' was not found in the local database'));
             array_splice($oracle, $key, 1);
         }
         $count++;
@@ -94,9 +84,31 @@ function causfa_oracle_compare($oracle) {
         send_message($row['FZVFORG_PTAG'], $count. ' of '.$total. " was ".$found_text, $percent);
     }
     send_message(0,'CLOSE', 'Process complete');
+    $wpdb->insert(
+        'causfa_exception_reports',
+        array(
+            'TOTAL_BANNER' => $total,
+            'TOTAL_LOCAL' => $total_local,
+            'EXCEPTIONS' => $exceptions_list
+        )
+    );
 }
 function causfa_oracle_compare_custodian($oracle, $asset) {
-    return '';
+    global $wpdb;
+    if ($oracle['FZVFORG_CUSTODIAN'] !== $asset->FZVFORG_CUSTODIAN) {
+        $result = $wpdb->get_row("SELECT * FROM causfa_pending WHERE FZVFORG_PTAG = '".$oracle['FZVFORG_PTAG']."';");
+        if ($result->PENDING_TYPE === 0 ) {
+            if ($result->PENDING_STATUS !== 7) {
+                return $result->FZVFORG_PTAG. ' is part of a pending transfer that has not been completed. The transfer is from '.causfa_email_get_name($result->PID_ORIGIN).' to '.causfa_email_get_name($result->PID_DESTINATION);
+            }
+        } else {
+            if ($result->PENDING_STATUS !== 5) {
+                return $result->FZVFORG_PTAG. ' is part of a pending surplus that has not been completed. The surplus was initiated by '.causfa_email_get_name($result->PID_ORIGIN);
+            }
+        }
+    } else {
+        return null;
+    }
 }
 function causfa_oracle_compare_location($oracle, $asset) {
     return '';
