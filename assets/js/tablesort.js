d21f2f7d39c719696fff5f352404fd80b2951ee5
Robert Vaughan <robertav@vt.edu>
2018-11-13 13:23:51 -0500

    Revert "Revert "tablesorter issues""
    This reverts commit 516bd11c5c1eba14ba23af34ab5d788c0c818008.

--- assets/js/tablesort.js
+++ assets/js/tablesort.js
@@ -1,29 +1,28 @@
-// $(document).ready(function() {
-//     alert("BLLLLLLAAAAAAAHHHHHH");
-//     if ($('#asset-table').length > 0) {
-//
-//         $("table").tablesorter();
-//
-//         $("#ajax-append").click(function() {
-//
-//             $.get("assets/ajax-content.html", function(html) {
-//
-//                 // append the "ajax'd" data to the table body
-//                 $("table tbody").append(html);
-//
-//                 // let the plugin know that we made a update
-//                 // the resort flag set to anything BUT false (no quotes) will trigger an automatic
-//                 // table resort using the current sort
-//                 var resort = true;
-//                 $("table").trigger("update", [resort]);
-//
-//                 // triggering the "update" function will resort the table using the current sort; since version 2.0.14
-//                 // use the following code to change the sort; set sorting column and direction, this will sort on the first and third column
-//                 // var sorting = [[2,1],[0,0]];
-//                 // $("table").trigger("sorton", [sorting]);
-//             });
-//
-//             return false;
-//         });
-//     }
-// });
+ $(document).ready(function() {
+     if ($('#asset-table').length > 0) {
+
+         $("table").tablesorter();
+
+         $("#ajax-append").click(function() {
+
+             $.get("assets/ajax-content.html", function(html) {
+
+                 // append the "ajax'd" data to the table body
+                 $("table tbody").append(html);
+
+                 // let the plugin know that we made a update
+                 // the resort flag set to anything BUT false (no quotes) will trigger an automatic
+                 // table resort using the current sort
+                 var resort = true;
+                 $("table").trigger("update", [resort]);
+
+                 // triggering the "update" function will resort the table using the current sort; since version 2.0.14
+                 // use the following code to change the sort; set sorting column and direction, this will sort on the first and third column
+                 // var sorting = [[2,1],[0,0]];
+                 // $("table").trigger("sorton", [sorting]);
+             });
+
+             return false;
+         });
+     }
+ });
